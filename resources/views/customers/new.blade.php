4927b46234d6854ffd78d64fb92ae31316ccba81
B00145951 Sophie Daly <B00145951@mytudublin.ie>
2024-02-12 16:47:59 +0000

    customers

---
+++ resources/views/customers/new.blade.php
@@ -0,0 +1,5 @@
+<FORM method="POST" action="/customers/create"> @csrf 
+     Enter your first name:<input type="text" name="firstname"><br> 
+     Enter your surname:<input type="text" name="surname"><br> 
+     <input type="submit"> 
+</FORM> 
