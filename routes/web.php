303741000750aacaa994c6b8c0e6d12d4911085c
B00145951 Sophie Daly <B00145951@mytudublin.ie>
2024-02-26 15:15:49 +0000

    login and logout, and register working

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
 Route::resource('members', App\Http\Controllers\memberController::class);
 
 Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
